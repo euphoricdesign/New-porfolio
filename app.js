b759a128f0c7feadc22e38986d12cc7bbe8f0438
euphoricdesign <noahchukita@gmail.com>
2021-04-15 19:15:19 -0300

    changes in contact section

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const hamburgerItems = document.querySelector('.hamburgerItems');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header,.container');
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+hamburgerItems.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
